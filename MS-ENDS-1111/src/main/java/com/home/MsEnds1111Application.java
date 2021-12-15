47afd27b85e418c424dff9e05aeef1e3add736cf
rahulbaliyan404 <rahulbaliyan404@gmail.com>
2021-12-15 22:24:59 +0530

    Comment added in main method of Eureka Netflix Discovery Server 15-12-2021.

--- MS-ENDS-1111/src/main/java/com/home/MsEnds1111Application.java
+++ MS-ENDS-1111/src/main/java/com/home/MsEnds1111Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class MsEnds1111Application {
-
+// This is Eureka Netflix Discovery Server.
 	public static void main(String[] args) {
 		SpringApplication.run(MsEnds1111Application.class, args);
 	}
